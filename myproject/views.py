476e607287290f0d0757c78ca8e16535b9062bad
kamilyte <kamilekaskelyte@gmail.com>
2023-09-12 13:50:12 +0200

    edit

--- myproject/views.py
+++ myproject/views.py
@@ -4,14 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     context = {
-        'name' : 'Kamile',
-        'age' : 20,
-        'nationality' : 'Lit'
+        'name' : 'World'
     }
 
     return render(request, 'index.html', context)
 
 def printCSV(request):
-    
     print("Title,Authors,Year,Citations\n")
-    
+
+def themisInput(request):
+    search = input()
+    name = input()
+    selfCitations = input()
+
